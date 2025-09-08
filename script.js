0b0837ee79b3eb9c2dcf07eacdfba5112710826b
REISINSO2024 <motoh5018@gmail.com>
2025-09-08 19:47:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const url = 'https://docs.google.com/spreadsheets/d/1UCXA6OSjfmoHWPp_OUMIZpHHWKK7eL-Ywqg-yLsMOFs/export?format=csv&gid=1242024225';
+let dados = [];
+
+fetch(url)
+  .then(response => response.text())
+  .then(csv => {
+    const linhas = csv.split('\n').map(l => l.split(','));
+    const cabecalho = linhas[0];
+    dados = linhas.slice(1).map(linha => {
+      const obj = {};
+      cabecalho.forEach((coluna, i) => {
+        obj[coluna.trim()] = linha[i]?.trim();
+      });
+      return obj;
+    });
+
+    const bairros = [...new Set(dados.map(item => item['BAIRRO']))];
+    const select = document.getElementById('bairroSelect');
+    bairros.forEach(bairro => {
+      if (bairro) {
+        const option = document.createElement('option');
+        option.value = bairro;
+        option.textContent = bairro;
+        select.appendChild(option);
+      }
+    });
+  });
+
+function filtrarPorBairro() {
+  const bairroSelecionado = document.getElementById('bairroSelect').value;
+  const tabela = document.getElementById('tabela-dados');
+  tabela.innerHTML = '';
+
+  const filtrados = dados.filter(item => item['BAIRRO'] === bairroSelecionado);
+
+  filtrados.forEach(item => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+      <td>${item['BAIRRO']}</td>
+      <td>${item['CÓDIGO']}</td>
+      <td>${item['ANO']}</td>
+      <td>${item['LIRA 1 QT']}</td>
+      <td>${item['LIRA 2 QT']}</td>
+      <td>${item['LIRA 3 QT']}</td>
+      <td>${item['LIRA 4 QT']}</td>
+      <td>${item['IIP 1 LIRAa']}</td>
+      <td>${item['IIP 2 LIRAa']}</td>
+      <td>${item['IIP 3 LIRAa']}</td>
+      <td>${item['IIP 4 LIRAa']}</td>
+    `;
+    tabela.appendChild(tr);
+  });
+}
